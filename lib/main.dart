8ec1031300ac6ac565ac301ca726f61492cd7827
janilso <janilsorodrigues10@gmail.com>
2018-12-17 16:19:13 -0300

    Finalizado contador de pessoas

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,87 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: "Contador de Pessas",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text("Pessoas 0",
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: FlatButton(
-                  child: Text("+1",
-                      style: TextStyle(color: Colors.white, fontSize: 30.0)),
-                  onPressed: () {}, // Função a ser chamada
+  runApp(MaterialApp(title: "Contador de Pessas", home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _pessoas = 0;
+  String _info = "Pode Entrar";
+  void _aumentaPessoa(int delta) {
+    setState(() {
+      _pessoas += delta;
+      if(_pessoas < 0) {
+        _info = "Mundo invertido?!";
+      } else if(_pessoas <= 10){
+        _info = "Pode Entrar!";
+      } else{
+        _info = "Lotado!";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "imagens/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1100.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Pessoas $_pessoas",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.none)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text("+1",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30.0,
+                            decoration: TextDecoration.none)),
+                    onPressed: () {
+                      _aumentaPessoa(1);
+                    }, // Função a ser chamada
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: FlatButton(
-                  child: Text("-1",
-                      style: TextStyle(color: Colors.white, fontSize: 30.0)),
-                  onPressed: () {}, // Função a ser chamada
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text("-1",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30.0,
+                        )),
+                    onPressed: () {
+                      _aumentaPessoa(-1);
+                    }, // Função a ser chamada
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Text("Pode entrar",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30.0)),
-        ],
-      )));
+              ],
+            ),
+            Text("$_info",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0,
+                    decoration: TextDecoration.none)),
+          ],
+        )
+      ],
+    );
+  }
 }
